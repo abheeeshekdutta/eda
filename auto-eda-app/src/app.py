771919c09c140cd1a3f4943a8464f6e5f59d0717
Abhishek Dutta <abhishek.dutta1337@gmail.com>
2023-07-22 19:23:15 +0100

    Updated code for the app

---
+++ auto-eda-app/src/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+st.title("📊 Automated Data Profiling Report")
+
+file = st.file_uploader("Upload your dataset here", type=["csv", "xlsx"])
+if file:
+    if file.name.split(".")[1] == "xlsx":
+        xl = pd.ExcelFile(file)
+        sheet_choice = st.selectbox(
+            "The following sheets were found in the Excel file. Choose one sheet",
+            (xl.sheet_names),
+        )
+        if st.button("Generate report"):
+            df = pd.read_excel(file, sheet_name=sheet_choice)
+            df.to_excel("./data/sourcedata.xlsx", index=None)
+            profile_report = ProfileReport(df)
+            st_profile_report(profile_report)
+    else:
+        df = pd.read_csv(file)
+        df.to_csv("./data/sourcedata.csv", index=None)
+        profile_report = ProfileReport(df)
+        st_profile_report(profile_report, navbar=True, height=1300)
